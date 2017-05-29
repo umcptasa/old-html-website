300c3acae339087fa9e40a46a721d6f1da46dc85
SLoh4137 <sloh4137@gmail.com>
2017-05-29 01:51:47 -0400

    Added love language quiz
    Love language quiz based off of Dr. Gary Chapman's "The Five Love
    Languages"
    Not yet accessible on the page. Have to manually put in url

---
+++ javascript/quiz_single.js
@@ -0,0 +1,158 @@
+var pos = 0;
+var lng_A = 0, lng_B = 0, lng_C = 0, lng_D = 0, lng_E = 0;
+
+const questions = [
+    ["someone I love sends me a loving note/text/email for no special reason", "I hug someone I love",
+        "A", "E"],
+    ["I can spend alone time with someone I love - just us","someone I love does something practical to help me out",
+        "B", "D"],
+    ["someone I love gives me a little gift as a token of our love of concern for each other", "I get to spend uninterrupted lesisure time with those I love",
+        "C", "B"],
+    ["someone I love does something unexpected for me to help me with a project", "I can share an innocent touch with someone I love",
+        "D", "E"],
+    ["someone I love puts their arm around me in public", "someone I love surprises me with a gift",
+        "E", "C"],
+    ["I'm around someone I love, even if we're not really doing anything", "I can be comfortable holding hands, high-fiving, or putting my arm around someone I love",
+        "B", "E"],
+    ["I receive a gift from someone I love", "I hear from someone I love that they love me",
+        "C", "A"],
+    ["I sit close to someone I love", "I am complimented by someone I love for no apparent reason",
+            "E", "A"],
+    ["I get the change to just \"hang out\" with someone I love", "I unexpectedly get small gifts from someone I love",
+        "B", "C"],
+    ["I hear someone I love tell me, \"I'm proud of you\"", "someone I love helps me with a task",
+        "A", "D"],
+    ["I get to do things with someone I love", "I hear supportive words from someone I love",
+        "B", "A"],
+    ["someone I love does things for me instead of just talking about doing nice things", "I feel connected to someone I love through a hug",
+        "D", "E"],
+    ["I hear praise from someone I love", "someone I love gives me something that shows they were really thinking about me",
+        "A", "C"],
+    ["I'm able to just be around someone I love", "I get a back rub from someone I love",
+        "B", "E"],
+    ["someone I love reacts positively to something I've accomplished", "someone I love does something for me that I know they don't particularly enjoy",
+        "A", "D"],
+    ["I'm able to be in close physical proximity to someone I love", "I sense someone I love showing interest in the things I care about",
+        "E", "B"],
+    ["someone I love works on special projects with me that I have to complete", "someone I love gives me an exciting gift",
+        "D", "C"],
+    ["I'm complimented by someone I love on my appearance", "someone I love takes the time to listen to me and really understands my feelings",
+        "A", "B"],
+    ["I can share a meaningful touch in public with someone I love", "someone I love offers to run errands for me",
+        "E", "D"],
+    ["someone I love does something special for me to help me out", "I get a gift that someone I love put thought into choosing",
+        "D", "C"],
+    ["someone I love doesn't check their phone while we're talking with each other", "someone I love goes out of their way to do something that relieves pressure on me",
+        "B", "D"],
+    ["I can look forward to a holiday because I'll probably get a gift from someone I love", "I hear the words, \"I appreciate you\" from someone I love",
+        "C", "A"],
+    ["someone I love and haven't seen in a while thinks enough of me to give me a little gift", "someone I love takes care of something I'm responsible to do that I feel too stressed to do at the time",
+        "C", "D"],
+    ["someone I love doesn't interrupt me while I'm talking", "gift giving is an important part of the relationship with someone I love",
+        "B", "C"],
+    ["someone I love helps me out when they know I'm already tired", "I get to go somewhere while spending time with someone I love",
+        "D", "B"],
+    ["someone I love touches my arm or shoulder to show their care or concern", "someone I love gives me a little gift that they picked up in the course of their normal day",
+        "E", "C"],
+    ["someone I love says something encouraging to me", "I get to spend time in a shared activity or hobby with someone I love",
+        "A", "B"],
+    ["someone I love surprises me with a small token of their appreciation", "I'm touching someone I love frequently to express our friendship",
+        "C", "E"],
+    ["someone I love helps me out - especially if I know they're already busy", "I hear someone I love tell me that they appreciate me",
+        "D", "A"],
+    ["I get a hug from someone whom I haven't seen in a while", "I hear someone I love tell me how much I mean to them",
+        "E", "A"],
+];
+
+function startQuiz() {
+
+    $("#test").replaceWith("<div id='test'><h3><i>It's more meaningful to me when</i></h3> <form id='questions'></form> <a class='myButton' id='next' onclick='nextQuestion()'>Next Question</a><div id='error'></div></div>");
+    $("#test").css("text-align", "left");
+    $("#error").css({
+        "text-align":"left",
+        "font-weight":"bold",
+        "margin": "1em"
+    });
+
+    renderQuestion();
+}
+
+function renderQuestion() {
+    var choice1 = questions[pos][0];
+    var choice2 = questions[pos][1];
+    var value1 = questions[pos][2];
+    var value2 = questions[pos][3];
+    var length = questions.length;
+
+    $('#test-status').text("Question " + (pos + 1) + " of " + length);
+    $('#error').text("");
+
+    $('#questions').replaceWith("<form id='questions'><input type='radio' name='choices' value='" + value1 +"' id='c1'/> <label for='c1'><span><span></span></span>" + choice1 +
+        "</label><br> <input type = 'radio' name = 'choices' value='" + value2 + "' id='c2'/> <label for='c2'><span><span></span></span>" + choice2 + "</label><br></form>");
+
+    if(pos == length - 1) {
+        $('#next').text("Finish");
+    }
+
+}
+
+function nextQuestion() {
+    var answer = $('input[name=choices]:checked', "#questions").val();
+
+    switch(answer) {
+        case 'A':
+            lng_A++;
+            break;
+        case 'B':
+            lng_B++;
+            break;
+        case 'C':
+            lng_C++;
+            break;
+        case 'D':
+            lng_D++;
+            break;
+        case 'E':
+            lng_E++;
+            break;
+        default:
+            $('#error').text("Please select an answer");
+            return;
+    }
+
+    if(pos != questions.length - 1) {
+        pos++;
+        renderQuestion();
+    } else {
+        finishQuiz();
+    }
+}
+
+function finishQuiz() {
+    var counter = 0;
+    var results = [
+        {name:"Words of Affirmation", value: lng_A, description:
+            "Actions don’t always speak louder than words. If this is your love language, unsolicited compliments mean the world to you. Hearing the words, \“I love you,\” are important—hearing the reasons behind that love sends your spirits skyward. Insults can leave you shattered and are not easily forgotten."},
+        {name:"Quality Time", value: lng_B, description:
+            "In the vernacular of Quality Time, nothing says, \“I love you,\” like full, undivided attention. Being there for this type of person is critical, but really being there—with the TV off, fork and knife down, and all chores and tasks on standby—makes your significant other feel truly special and loved. Distractions, postponed dates, or the failure to listen can be especially hurtful."},
+        {name:"Receiving Gifts", value: lng_C, description:
+            "Don’t mistake this love language for materialism; the receiver of gifts thrives on the love, thoughtfulness, and effort behind the gift. If you speak this language, the perfect gift or gesture shows that you are known, you are cared for, and you are prized above whatever was sacrificed to bring the gift to you. A missed birthday, anniversary, or a hasty, thoughtless gift would be disastrous—so would the absence of everyday gestures."},
+        {name:"Acts of Service", value: lng_D, description:
+            "Can vacuuming the floors really be an expression of love? Absolutely! Anything you do to ease the burden of responsibilities weighing on an \“Acts of Service\” person will speak volumes. The words he or she most wants to hear: \“Let me do that for you.\” Laziness, broken commitments, and making more work for them tell speakers of this language their feelings don’t matter."},
+        {name:"Physical Touch", value: lng_E, description:
+            "This language isn’t all about the bedroom. A person whose primary language is Physical Touch is, not surprisingly, very touchy. Hugs, pats on the back, holding hands, and thoughtful touches on the arm, shoulder, or face—they can all be ways to show excitement, concern, care, and love. Physical presence and accessibility are crucial, while neglect or abuse can be unforgivable and destructive."}
+    ];
+
+    results.sort(function(a, b) {
+        return b.value - a.value;
+    });
+
+    $('#test-status').text("Your love language is: " + results[0].name);
+
+    $("#test").replaceWith("<div id='test-results'></div>");
+
+    for(counter = 0; counter < results.length; counter++) {
+        $("#test-results").append("<h2>" + (counter + 1) + ". " + results[counter].name +
+            " " + results[counter].value + "</h2><br><h3>" + results[counter].description + "</h3><br>");
+    }
+}
