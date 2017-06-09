15bd7c8653c7dacfd51fac4e9d21b9e98ef52b60
SLoh4137 <sloh4137@gmail.com>
2017-06-09 01:18:01 -0400

    Blacked out inactive members
    Began implementing the different tabs for different founders of lines
    but encountering problems with selecting dynamically created ids

--- javascript/familyTree.js
+++ javascript/familyTree.js
@@ -3,17 +3,50 @@
 * Use http://fperucic.github.io/treant-js/
 */
 var FAMILY_TREE = new Map();
+var FOUNDERS = [];
 
 $(document).ready(function() {
     Tabletop.init({
         key: '1q0ctDiufN-Fzg59HC5EZ5pWbVyiondZOxyG2ij3Es2g',
         callback: function(data, tabletop) {
             data.forEach(addPerson);
-            renderTree();
+            updateChildren("Founder");
+
+            /*
+            FAMILY_TREE.get("Founder").children.forEach(function(lineFounder) {
+                createTab(lineFounder.text.name);
+            });
+            */
+            
+            var familyTreeConfig = {
+            	chart: {
+            		container: "#Founder",
+            		animateOnInit: true,
+                    scrollbar:"fancy",
+                    siblingSeparation: 10,
+                    hideRootNode: true,
+            		node: {
+            			collapsable: true
+            		},
+            		animation: {
+            			nodeAnimation: "easeOutBounce",
+            			nodeSpeed: 700,
+            			connectorsAnimation: "bounce",
+            			connectorSpeed: 700
+            		}
+            	},
+
+            	nodeStructure: FAMILY_TREE.get("Founder")
+            };
+
+            var tree = new Treant(familyTreeConfig, function() {
+                    console.log("Tree for Founder loaded");
+            }, $);
 
         },
         simpleSheet: true
     });
+
 });
 
 function addPerson(person) {
@@ -21,31 +54,43 @@ function addPerson(person) {
     big = person.Big;
     littles = person.Littles.split(",");
 
-    console.log(big + " found in " + FAMILY_TREE.has(big));
+    //console.log(big + " found in " + FAMILY_TREE.has(big));
 	if(FAMILY_TREE.has(big)) {
         //console.log("Found: " + big);
 		parentNode = FAMILY_TREE.get(big);
 	} else {
         //console.log(big);
+        if(big.charAt(0) == '(') {
+            parentNode = new Node(big, "inactive");
+        } else {
+            parentNode = new Node(big, "active");
+        }
 		parentNode = new Node(big);
 		FAMILY_TREE.set(big, parentNode);
 	}
 
 	/* Children is an array of Strings */
 	littles.forEach(function(c) {
-        console.log("Little for " + big + " is " + c.trim());
-		var child  = new Node(c.trim());
-        FAMILY_TREE.set(c.trim(), child);
+        //console.log("Little for " + big + " is " + c.trim());
+        var childName = c.trim(), child;
+        if(childName.charAt(0) == '(') {
+            child = new Node(childName, "inactive");
+        } else {
+            child = new Node(childName, "active");
+        }
+
+        FAMILY_TREE.set(childName, child);
 
         parentNode.children.push(child);
 		//parentNode.addChild(child);
 	});
 }
 
-function Node(name) {
+function Node(name, htmlClass) {
 	this.text = {name: name };
 	this.children = [];
 	this.HTMLid = name;
+    this.HTMLclass = htmlClass;
     //this.parent = parent;
 }
 
@@ -67,14 +112,22 @@ function updateChildren(name) {
     }
 }
 
-function renderTree() {
-    updateChildren("Founder");
-    var familyTreeConfig = {
-    	chart: {
-    		container: "#familyTree",
+function createTab(lineFounder) {
+    console.log("HERE " + lineFounder);
+    $('#tabs').append("<button class='tablinks' onclick='renderTree(event, " + lineFounder + ")'>" + lineFounder + "</button>");
+    $('#families').append("<div class='tabContent chart' val='hi' id='" + lineFounder + "'></div>");
+    createTree(lineFounder);
+}
+
+function createTree(lineFounder) {
+    console.log($('#' + lineFounder).val());
+    var config = {
+        chart: {
+    		container: "#" + lineFounder,
     		animateOnInit: true,
             scrollbar:"fancy",
             siblingSeparation: 10,
+            hideRootNode: false,
     		node: {
     			collapsable: true
     		},
@@ -86,11 +139,16 @@ function renderTree() {
     		}
     	},
 
-    	nodeStructure: FAMILY_TREE.get("Founder")
+    	nodeStructure: FAMILY_TREE.get(lineFounder)
     };
-
-    var tree = new Treant(familyTreeConfig, function() {
-            console.log("tree loaded");
+    var tree = new Treant(config, function(){
+        console.log("Tree for " + lineFounder + " loaded");
     }, $);
+}
 
+function renderTree(event, lineFounder) {
+    $(".tabContent").hide();
+    $(".tablinks").removeClass("active");
+    $("#" + lineFounder).show();
+    event.currentTarget.className += "active";
 }
