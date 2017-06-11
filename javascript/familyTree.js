6dff4983a5f401be31ba532b15d0f24528eead6b
SLoh4137 <sloh4137@gmail.com>
2017-06-11 15:58:05 -0400

    Added tabs for each founder in the family tree
    Each founder now has their own tab in the family tree, making looking
    through one family much cleaner. Page is also now accessible on the
    website

--- javascript/familyTree.js
+++ javascript/familyTree.js
@@ -3,8 +3,17 @@
 * Use http://fperucic.github.io/treant-js/
 */
 var FAMILY_TREE = new Map();
-var FOUNDERS = [];
+var VISIBLE = "Founder";
 
+/*
+* Called once all of the scripts are done loading
+* Initializes Tabletop.js which pulls data from the Google Sheet
+* Then for each row, create a person via addPerson
+* Once that is done, go through the map once more and update the references to children
+* Then for each of the Founder node's children, create an HTML tab and tree for it
+* Create the family tree via Treant js for the founder node
+* Hide all of the trees just created, and show only the Founder node
+*/
 $(document).ready(function() {
     Tabletop.init({
         key: '1q0ctDiufN-Fzg59HC5EZ5pWbVyiondZOxyG2ij3Es2g',
@@ -12,12 +21,12 @@ $(document).ready(function() {
             data.forEach(addPerson);
             updateChildren("Founder");
 
-            /*
             FAMILY_TREE.get("Founder").children.forEach(function(lineFounder) {
-                createTab(lineFounder.text.name);
+                var name = lineFounder.text.name;
+                createTab(name, name.replace(" ", ""));
             });
-            */
-            
+
+
             var familyTreeConfig = {
             	chart: {
             		container: "#Founder",
@@ -40,38 +49,45 @@ $(document).ready(function() {
             };
 
             var tree = new Treant(familyTreeConfig, function() {
-                    console.log("Tree for Founder loaded");
+                    //console.log("Tree for Founder loaded");
             }, $);
-
+            $(".tabcontent").hide();
+            $("#Founder").show();
         },
         simpleSheet: true
     });
 
 });
 
+/*
+* Adds a person to the Map
+* Row gotten from Tabletop js consists of Big column and Littles column
+* If the FAMILY_TREE map already has the big, then get that node
+* Otherwise, create one and put it into the map with name as the key
+*
+* For each of the children, trim the name so remove any spaces
+* If the child has ( as first character, represents person is inactive
+* Add the node to the map
+* Add this new node to the big's children array
+*/
 function addPerson(person) {
 	var big, littles, parentNode;
     big = person.Big;
     littles = person.Littles.split(",");
 
-    //console.log(big + " found in " + FAMILY_TREE.has(big));
 	if(FAMILY_TREE.has(big)) {
-        //console.log("Found: " + big);
 		parentNode = FAMILY_TREE.get(big);
 	} else {
-        //console.log(big);
         if(big.charAt(0) == '(') {
             parentNode = new Node(big, "inactive");
         } else {
             parentNode = new Node(big, "active");
         }
-		parentNode = new Node(big);
 		FAMILY_TREE.set(big, parentNode);
 	}
 
 	/* Children is an array of Strings */
 	littles.forEach(function(c) {
-        //console.log("Little for " + big + " is " + c.trim());
         var childName = c.trim(), child;
         if(childName.charAt(0) == '(') {
             child = new Node(childName, "inactive");
@@ -82,18 +98,23 @@ function addPerson(person) {
         FAMILY_TREE.set(childName, child);
 
         parentNode.children.push(child);
-		//parentNode.addChild(child);
 	});
 }
 
+/*
+* Basic data structure stored in the Map and used for the tree
+*/
 function Node(name, htmlClass) {
-	this.text = {name: name };
+	this.text = {name: name};
 	this.children = [];
-	this.HTMLid = name;
+	this.HTMLid = name.replace(" ", "_");
     this.HTMLclass = htmlClass;
     //this.parent = parent;
 }
 
+/*
+* Recursively goes through the current node's children and updates references to them
+*/
 function updateChildren(name) {
     var children, length = 0;;
     if(FAMILY_TREE.has(name)) {
@@ -112,18 +133,27 @@ function updateChildren(name) {
     }
 }
 
-function createTab(lineFounder) {
-    console.log("HERE " + lineFounder);
-    $('#tabs').append("<button class='tablinks' onclick='renderTree(event, " + lineFounder + ")'>" + lineFounder + "</button>");
-    $('#families').append("<div class='tabContent chart' val='hi' id='" + lineFounder + "'></div>");
-    createTree(lineFounder);
+/*
+* Creates a tab in the HTML
+* <button class='tablinks' onclick='renderTree(event, "lineFounderNoSpace")'>lineFounder</button>
+* <div class='tabcontent chart' id='lineFounderNoSpace'></div>
+* Then once the HTML is set, creates a tree linking to it
+*/
+function createTab(lineFounder, lineFounderNoSpace) {
+    $('#allTabs').append("<button class='tablinks' onclick='renderTree(event, \"" + lineFounderNoSpace + "\")'>" + lineFounder + "</button>");
+    $('#families').append("<div class='tabcontent chart' id='" + lineFounderNoSpace + "'></div>");
+
+    createTree(lineFounder, lineFounderNoSpace);
 }
 
-function createTree(lineFounder) {
-    console.log($('#' + lineFounder).val());
+/*
+* Creates a Treant.js tree
+*/
+function createTree(lineFounder, lineFounderNoSpace) {
+    var selector = "#" + lineFounderNoSpace;
     var config = {
         chart: {
-    		container: "#" + lineFounder,
+    		container: selector,
     		animateOnInit: true,
             scrollbar:"fancy",
             siblingSeparation: 10,
@@ -142,13 +172,18 @@ function createTree(lineFounder) {
     	nodeStructure: FAMILY_TREE.get(lineFounder)
     };
     var tree = new Treant(config, function(){
-        console.log("Tree for " + lineFounder + " loaded");
+        //console.log("Tree for " + lineFounder + " loaded");
     }, $);
 }
 
-function renderTree(event, lineFounder) {
-    $(".tabContent").hide();
-    $(".tablinks").removeClass("active");
-    $("#" + lineFounder).show();
-    event.currentTarget.className += "active";
+/*
+* Functionality for clicking on the tab
+* hides the currently visible family tree and shows the new one
+*/
+function renderTree(event, lineFounderNoSpace) {
+    $("#" + VISIBLE).hide();
+    //$(".tablinks").removeClass("active");
+    //event.currentTarget.className += "active";
+    VISIBLE = lineFounderNoSpace;
+    $("#" + lineFounderNoSpace).show();
 }
