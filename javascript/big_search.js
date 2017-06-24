d4a7ae8023166959b8eb766d721fc287e4e225d9
SLoh4137 <sloh4137@gmail.com>
2017-06-24 14:16:21 -0400

    Added search for bigs feature

--- javascript/big_search.js
+++ javascript/big_search.js
@@ -1,85 +1,120 @@
-var ALL_GROUPS = new Map();
-var ALL_PEOPLE = new Array();
+var ALL_GROUPS = [];
+var ALL_MAJORS = [];
+var ALL_PEOPLE = [];
+
+/**
+* Pulls data from the GoogleSheet
+* Then fills the table
+* Fills the majors and the groups used in the filter
+**/
 $(document).ready(function() {
     Tabletop.init({
-        key: 'https://docs.google.com/spreadsheets/d/1EqqgCV03NuBFQnEJTJ2iLpu5UjxG0ZIsRb8X9b20kqc/pubhtml',
+        key: '1EqqgCV03NuBFQnEJTJ2iLpu5UjxG0ZIsRb8X9b20kqc',
         callback: function(data, tabletop) {
             data.forEach(addPerson);
             fillTable();
-
+            fillMajors();
+            fillGroups();
         },
         simpleSheet: true
     });
-    /*
-    var allPeople = [
-        new Person("Stephan", "Computer Science", "Sophomore",
-            "North Potomac, MD", "Prince Frederick", "ACES, UMSO", "Physical Touch, Quality Time")
-
-    ];
-
-    fillTable(allPeople);
-    //fillTableFromSheet() ;
-    */
 });
 
+/*
+* Person object
+* Calls parseStudentGroups in order to split the groups and add them to the global array
+*/
 function Person(name, major, year, dorm, studentGroups, loveLanguages) {
     this.name = name;
-    this.major = major;
+    this.id = name.replace(" ", "_");
+    this.majorText = major;
+    this.major = parseMajor(major);
     this.year = year;
     this.dorm = dorm;
-    this.studentGroupsText = studentGroups
+    this.studentGroupsText = studentGroups;
     this.studentGroups = parseStudentGroups(studentGroups);
     this.loveLanguagesText = loveLanguages;
     this.loveLanguages = loveLanguages.split(",");
 }
 
+/*
+* Splits the string between the commas, and returns an array containing those values
+* Adds all of the individual student groups into the global array ALL_MAJORS
+*/
+function parseMajor(majorText) {
+     var arrayMajor = majorText.split(",");
+
+     arrayMajor.forEach(function(major) {
+        //Checks if the group already exists in the array
+        if(ALL_MAJORS.indexOf(major) == -1) {
+            ALL_MAJORS.push(major);
+        }
+     });
+
+     return arrayMajor;
+}
+
+/*
+* Splits the string between the commas, and returns an array containing those values
+* Adds all of the individual student groups into the global array ALL_GROUPS
+*/
 function parseStudentGroups(studentGroups) {
      var arrayGroups = studentGroups.split(",");
 
      arrayGroups.forEach(function(group) {
-        var key = group.toUpperCase();
-        if(ALL_GROUPS.has(key)) {
-            ALL_GROUPS.set(key, ALL_GROUPS.get(key) + 1);
-        } else {
-            ALL_GROUPS.set(key, 1);
+        //Checks if the group already exists in the array
+        if(ALL_GROUPS.indexOf(group) == -1) {
+            ALL_GROUPS.push(group);
         }
      });
 
      return arrayGroups;
 }
 
+
+/*
+* Called from the Tabletop initialization. Called on each row in the GoogleSheet
+* Creates a new person from the information provided and pushes the new person to the global array ALL_PEOPLE
+*/
 function addPerson(row) {
     var person = new Person(row.Name, row.Major, row.Year, row.Dorm, row.Groups, row.LoveLanguage);
     ALL_PEOPLE.push(person);
 }
 
-
+/*
+* For every person in the array ALL_PEOPLE, create an entry for them in the table
+* Uses a Handlebars template declared in blsearch.html
+* Creates an overview for the person
+*/
 function fillTable() {
-    ALL_PEOPLE.forEach(createEntry);
-}
-
-function createEntry(person) {
     var entryTemplate = Handlebars.compile($("#entryTemplate").html());
 
-    var personHTML = entryTemplate(person);
-    $("#searchTable").append(personHTML);
+    ALL_PEOPLE.forEach(function(person) {
+        //Adds the entry to the table
+        var personHTML = entryTemplate(person);
+        $("#searchTable").append(personHTML);
 
-    /*
-    $("#searchTable").on("click", "#" + person.name, function() {
-        $("searchTable").on("click", "#" + person.name + "overview").show(function() {
-            document.body.addEventListener('click', closeOverview, false);
+        //Creates the overview for the person that'll appear onclick
+        $("#" + person.id).click(function(){
+            $("#" + person.id + "overview").show(function() {
+                document.body.addEventListener('click', closeOverview, false);
+            });
         });
     });
-    */
 }
 
+/*
+* Shows the overview for the person when clicked. Added in the HTML
+*/
 function showOverview(id) {
-    console.log(id);
     $(id).show(function() {
         document.body.addEventListener('click', closeOverview, false);
     })
 }
 
+/*
+* Closes all overviews when the user clicks on anything that isn't an overview
+*/
 function closeOverview(e) {
     if(e.target.class != "overview") {
         document.body.removeEventListener('click', closeOverview, false);
@@ -87,17 +122,87 @@ function closeOverview(e) {
     }
 }
 
+/*
+* Fills the filterMajor table with all of the majors
+* Uses a Handlebars template defined in blsearch.html
+*/
+function fillMajors() {
+    var majorTemplate = Handlebars.compile($("#majorTemplate").html());
 
+    ALL_MAJORS.forEach(function(major) {
+        var entryHTML = majorTemplate({
+            id: major.replace(" ", "_"),
+            name: major
+        });
+        $("#filterMajor").append(entryHTML);
+    });
+
+}
+
+/*
+* Fills the filterGroups table with all of the groups
+* Uses a Handlebars template defined in blsearch.html
+*/
+function fillGroups() {
+    var groupTemplate = Handlebars.compile($("#groupTemplate").html());
+
+    ALL_GROUPS.forEach(function(group) {
+        var entryHTML = groupTemplate({
+            id: group.replace(" ", "_"),
+            name: group
+        });
+        $("#filterGroups").append(entryHTML);
+    });
+
+}
+
+/*
+* Checks if the person's major is checked in the Advanced Options
+* Note: by default all of the majors are checked
+* As long as one of the majors is selected, then shown
+*/
+function majorChecked(person) {
+    var majors = person.major, length = majors.length;
+
+    for(i = 0; i < length; i++) {
+        if($("#" + majors[i].replace(" ", "_")).is(":checked")) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/*
+* Checks if the person's group is checked in the Advanced Options
+* Note: by default all of the groups are checked
+* As long as one of the groups is selected, then shown
+*/
+function groupChecked(person) {
+    var groups = person.studentGroups, length = groups.length;
+
+    for(i = 0; i < length; i++) {
+        if($("#" + groups[i].replace(" ", "_")).is(":checked")) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/*
+* The search filter
+* Called whenever the user types into the searchInput HTML element
+*/
 function filter() {
-  // Declare variables
   var input, arrayLength;
   input = $("#searchInput").val().toUpperCase();
 
   ALL_PEOPLE.forEach(function(person) {
-     if(person.name.toUpperCase().indexOf(input) > -1) {
-         $("#" + person.name).css("display", "table-row");
+     if(person.name.toUpperCase().indexOf(input) > -1 && majorChecked(person) && groupChecked(person)) {
+         $("#" + person.id).show();
      } else {
-         $("#" + person.name).css("display", "none");
+         $("#" + person.id).hide();
      }
   });
 
